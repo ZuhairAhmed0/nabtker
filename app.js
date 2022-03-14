bd96fdbb5103175d2bf2e21fa3db909f096d00a9
Raghad-Alamri <raghadlu999@gmail.com>
2022-03-14 14:50:21 +0200

    last update

--- app.js
+++ app.js
@@ -18,14 +18,13 @@ app.set('view engine', 'ejs');
 const DB_INFO = {
 	host: 'localhost',
 	user: 'root',
-	password: 'zuhairDB67&',
-	database: 'tests',
+	password: 'nabtkerDB67&',
+	database: 'nabtkerdb',
 	connectionLimit: 10,
 };
 
 //  للاتصال بقاعدة البيانات
-const pool = mysql.createPool(DB_INFO);
-const con = mysql.createConnection(DB_INFO);
+const conn = mysql.createConnection(DB_INFO);
 
 // الصفحة الرئسية
 app.get('/', (req, res) => {
@@ -42,11 +41,6 @@ app.get('/addNewAdmin', (req, res) => {
 	res.sendFile('/views/addNew.html', {root: __dirname});
 });
 
-//صفحة تفاصيل الكتاب
-// app.get('/details', (req, res) => {
-// 	res.sendFile('/views/details.html', {root: __dirname});
-// });
-
 // صفحة تسجيل الدخول
 app.get('/login', (req, res) => {
 	res.sendFile('/views/login.html', {root: __dirname});
@@ -58,11 +52,11 @@ app.post('/login', (req, res) => {
 	try {
 		var DBuserCheck = `SELECT EXISTS (SELECT * FROM admininfo WHERE username ="${username}")`;
 		var DBpwdCheck = `SELECT * FROM admininfo where username = "${username}" AND password = "${password}"`;
-		pool.query(DBuserCheck, function (err, data) {
+		conn.query(DBuserCheck, function (err, data) {
 			if (err) {
 				res.send(err);
 			} else {
-				pool.query(DBpwdCheck, function (err, userData) {
+				conn.query(DBpwdCheck, function (err, userData) {
 					res.json(userData[0]);
 				});
 			}
@@ -74,7 +68,7 @@ app.post('/login', (req, res) => {
 
 //  لجلب جميع الادمن من قاعدة البيانات
 app.get('/allAdmin', (req, res) => {
-	pool.query('select id, name, email from admininfo', (err, result, fields) => {
+	conn.query('select id, name, email from admininfo', (err, result, fields) => {
 		if (err) {
 			return err;
 		}
@@ -86,9 +80,9 @@ app.get('/allAdmin', (req, res) => {
 app.post('/addNewAdmin', (req, res) => {
 	if (req.body.name && req.body.username && req.body.email && req.body.password) {
 		let email = req.body.email;
-		con.connect((err) => {
+		conn.connect((err) => {
 			let sql = `INSERT into admininfo(name, username,email,password)values('${req.body.name}','${req.body.username}','${email}','${req.body.password}')`;
-			con.query(sql, (err, result) => {
+			conn.query(sql, (err, result) => {
 				if (err) throw err;
 				res.redirect('nabtker_dashboard');
 			});
@@ -100,10 +94,10 @@ app.post('/addNewAdmin', (req, res) => {
 app.put('/editAdminInfo/:id', (req, res) => {
 	if (req.body.name && req.body.username && req.body.email && req.body.password) {
 		let email = req.body.email;
-		con.connect((err) => {
+		conn.connect((err) => {
 			let sql = `UPDATE admininfo SET name = '${req.body.name}', username = '${req.body.username}', email = '${email}', password = '${req.body.password}' WHERE (id = '${req.params.id}');
 			`;
-			con.query(sql, (err, result) => {
+			conn.query(sql, (err, result) => {
 				if (err) throw err;
 				res.json('edit successfully');
 			});
@@ -113,12 +107,12 @@ app.put('/editAdminInfo/:id', (req, res) => {
 
 //  لحذف ادمن معين
 app.delete('/DeleteAdmin/:id', (req, res) => {
-	con.connect((err) => {
+	conn.connect((err) => {
 		let sql = `DELETE from admininfo where id = "${req.params.id}"`;
 		let sql2 = 'SELECT * from admininfo';
-		con.query(sql2, (err, checkAdmin) => {
+		conn.query(sql2, (err, checkAdmin) => {
 			if (checkAdmin.length != 1) {
-				con.query(sql, (err, result) => {
+				conn.query(sql, (err, result) => {
 					res.json({msg: 'deleted successfully', id: checkAdmin.length});
 				});
 			} else {
@@ -154,9 +148,9 @@ app.post(
 		const author = req.body.authorName;
 		const countPages = req.body.countPages;
 		const dateVersion = req.body.dateVersion;
-		con.connect((err) => {
+		conn.connect((err) => {
 			let sql = `INSERT into filesupload(fileSrc, fileName, fileImg, author, countPages, dateVersion)values('${fileSrc}','${fileName}','${fileImg}','${author}','${countPages}','${dateVersion}')`;
-			con.query(sql);
+			conn.query(sql);
 			res.redirect('nabtker_dashboard');
 		});
 	},
@@ -164,7 +158,7 @@ app.post(
 
 // لجلب بيانات الملف من قاعدة البيانات
 app.get('/uploadFiles', (req, res) => {
-	pool.query('select * from filesupload', (err, result, fields) => {
+	conn.query('select * from filesupload', (err, result, fields) => {
 		if (err) {
 			return err;
 		}
@@ -174,22 +168,22 @@ app.get('/uploadFiles', (req, res) => {
 
 //  لتعديل بيانات الملف في الصفحة وقاعدة البيانات
 app.post('/editFiles/:id', (req, res) => {
-	con.connect((err) => {
+	conn.connect((err) => {
 		let sql = `UPDATE filesupload SET fileName = "${req.body.new_fileName}", author = "${req.body.new_authorName}", countPages = "${req.body.new_countPages}", dateVersion = "${req.body.new_dateVersion}" WHERE (id = "${req.params.id}")`;
-		con.query(sql);
+		conn.query(sql);
 		res.redirect('/nabtker_dashboard');
 	});
 });
 
 // لحذف ملف معين
 app.delete('/DeleteFiles/:id', (req, res) => {
-	con.connect((err) => {
+	conn.connect((err) => {
 		let sql = `DELETE from filesupload where id = "${req.params.id}"`;
 		let deleteFile = `select fileSrc , fileImg from filesupload where id = "${req.params.id}"`;
-		con.query(deleteFile, (err, result_file, fields) => {
+		conn.query(deleteFile, (err, result_file, fields) => {
 			fs.unlink(`./public/uploads/${result_file[0].fileSrc}`, (err) => {
 				fs.unlink(`./public/uploads/${result_file[0].fileImg}`, () => {
-					con.query(sql);
+					conn.query(sql);
 				});
 			});
 			res.json('deleted successfully');
@@ -200,7 +194,7 @@ app.delete('/DeleteFiles/:id', (req, res) => {
 // لعرض تفاصيل الملف
 app.get('/details/:id', (req, res) => {
 	const detailsFile = `select * from filesupload where id=${req.params.id}`;
-	con.query(detailsFile, (err, result) => {
+	conn.query(detailsFile, (err, result) => {
 		res.render('details', {data: result});
 	});
 });
